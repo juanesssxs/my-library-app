1e8dfeaa3fd6df1344c73aa483791c09040e5b9c
vector18xd <victxd18pepe@gmail.com>
2024-06-13 08:33:12 -0500

    tipado primitivo

---
+++ src/variables-types.ts
@@ -0,0 +1,23 @@
+let bookTitle: string = "TypeScript Basics"
+let libraryName:string = "My library"
+let numberOfBooks: number = 1084331933
+let isLibraryOpen: boolean = true
+
+let titulo: string = "La Sirenita"
+let autor: string = "Hans Christian Andersen"
+let genero: string = "Ficcion"
+let idioma: string ="Español"
+let precio: number =15.000
+let descuento: string= "20%"
+let formato: string = "Tapa dura"
+let isbn: number = 29788478888566
+let descripcion: string = "La historia de Ariel, una sirena que sueña con vivir en tierra firme."
+let estado: string = "Nuevo"
+let ubicacion: string = "Los Angeles, Estados Unidos"
+let fecha_publicacion: string = "Junio 1837"
+let editorial: string = "Disney Press"
+let paginas: number = 48
+let dimensiones: string = "8.5 x 0.5 x 10 pulgadas" 
+let peso: string = "1.0 libras"
+let disponible: boolean = true
+
